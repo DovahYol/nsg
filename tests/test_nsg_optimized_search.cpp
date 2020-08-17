e289cbd724c81f8057a3fa8312ba2ceb96c56d46
bing zhao <t-bizhao@microsoft.com>
2020-08-18 02:26:27 +0800

    Add information about latency and recall

--- tests/test_nsg_optimized_search.cpp
+++ tests/test_nsg_optimized_search.cpp
@@ -8,85 +8,128 @@
 #include <string>
 
 void load_data(char* filename, float*& data, unsigned& num,
-               unsigned& dim) {  // load data with sift10K pattern
-  std::ifstream in(filename, std::ios::binary);
-  if (!in.is_open()) {
-    std::cout << "open file error" << std::endl;
-    exit(-1);
-  }
-  in.read((char*)&dim, 4);
-  // std::cout<<"data dimension: "<<dim<<std::endl;
-  in.seekg(0, std::ios::end);
-  std::ios::pos_type ss = in.tellg();
-  size_t fsize = (size_t)ss;
-  num = (unsigned)(fsize / (dim + 1) / 4);
-  data = new float[(size_t)num * (size_t)dim];
+	unsigned& dim) {  // load data with sift10K pattern
+	std::ifstream in(filename, std::ios::binary);
+	if (!in.is_open()) {
+		std::cout << "open file error" << std::endl;
+		exit(-1);
+	}
+	in.read((char*)&dim, 4);
+	// std::cout<<"data dimension: "<<dim<<std::endl;
+	in.seekg(0, std::ios::end);
+	std::ios::pos_type ss = in.tellg();
+	size_t fsize = (size_t)ss;
+	num = (unsigned)(fsize / (dim + 1) / 4);
+	data = new float[(size_t)num * (size_t)dim];
 
-  in.seekg(0, std::ios::beg);
-  for (size_t i = 0; i < num; i++) {
-    in.seekg(4, std::ios::cur);
-    in.read((char*)(data + i * dim), dim * 4);
-  }
-  in.close();
+	in.seekg(0, std::ios::beg);
+	for (size_t i = 0; i < num; i++) {
+		in.seekg(4, std::ios::cur);
+		in.read((char*)(data + i * dim), dim * 4);
+	}
+	in.close();
 }
 
 void save_result(const char* filename,
-                 std::vector<std::vector<unsigned> >& results) {
-  std::ofstream out(filename, std::ios::binary | std::ios::out);
+	std::vector<std::vector<unsigned> >& results) {
+	std::ofstream out(filename, std::ios::binary | std::ios::out);
 
-  for (unsigned i = 0; i < results.size(); i++) {
-    unsigned GK = (unsigned)results[i].size();
-    out.write((char*)&GK, sizeof(unsigned));
-    out.write((char*)results[i].data(), GK * sizeof(unsigned));
-  }
-  out.close();
+	for (unsigned i = 0; i < results.size(); i++) {
+		unsigned GK = (unsigned)results[i].size();
+		out.write((char*)&GK, sizeof(unsigned));
+		out.write((char*)results[i].data(), GK * sizeof(unsigned));
+	}
+	out.close();
 }
 int main(int argc, char** argv) {
-  if (argc != 7) {
-    std::cout << argv[0]
-              << " data_file query_file nsg_path search_L search_K result_path"
-              << std::endl;
-    exit(-1);
-  }
-  float* data_load = NULL;
-  unsigned points_num, dim;
-  load_data(argv[1], data_load, points_num, dim);
-  float* query_load = NULL;
-  unsigned query_num, query_dim;
-  load_data(argv[2], query_load, query_num, query_dim);
-  assert(dim == query_dim);
+	if (argc != 7 && argc != 8) {
+		std::cout << argv[0]
+			<< " data_file query_file nsg_path search_L search_K result_path"
+			<< std::endl;
 
-  unsigned L = (unsigned)atoi(argv[4]);
-  unsigned K = (unsigned)atoi(argv[5]);
+		std::cout << argv[0]
+			<< " data_file query_file nsg_path search_L search_K result_path truth_path"
+			<< std::endl;
+		exit(-1);
+	}
+	float* data_load = NULL;
+	unsigned points_num, dim;
+	load_data(argv[1], data_load, points_num, dim);
+	float* query_load = NULL;
+	unsigned query_num, query_dim;
+	load_data(argv[2], query_load, query_num, query_dim);
+	assert(dim == query_dim);
 
-  if (L < K) {
-    std::cout << "search_L cannot be smaller than search_K!" << std::endl;
-    exit(-1);
-  }
+	unsigned L = (unsigned)atoi(argv[4]);
+	unsigned K = (unsigned)atoi(argv[5]);
 
-  // data_load = efanna2e::data_align(data_load, points_num, dim);//one must
-  // align the data before build query_load = efanna2e::data_align(query_load,
-  // query_num, query_dim);
-  efanna2e::IndexNSG index(dim, points_num, efanna2e::FAST_L2, nullptr);
-  index.Load(argv[3]);
-  index.OptimizeGraph(data_load);
+	if (L < K) {
+		std::cout << "search_L cannot be smaller than search_K!" << std::endl;
+		exit(-1);
+	}
 
-  efanna2e::Parameters paras;
-  paras.Set<unsigned>("L_search", L);
-  paras.Set<unsigned>("P_search", L);
+	// data_load = efanna2e::data_align(data_load, points_num, dim);//one must
+	// align the data before build query_load = efanna2e::data_align(query_load,
+	// query_num, query_dim);
+	efanna2e::IndexNSG index(dim, points_num, efanna2e::FAST_L2, nullptr);
+	index.Load(argv[3]);
+	index.OptimizeGraph(data_load);
 
-  std::vector<std::vector<unsigned> > res(query_num);
-  for (unsigned i = 0; i < query_num; i++) res[i].resize(K);
+	efanna2e::Parameters paras;
+	paras.Set<unsigned>("L_search", L);
+	paras.Set<unsigned>("P_search", L);
 
-  auto s = std::chrono::high_resolution_clock::now();
-  for (unsigned i = 0; i < query_num; i++) {
-    index.SearchWithOptGraph(query_load + i * dim, K, paras, res[i].data());
-  }
-  auto e = std::chrono::high_resolution_clock::now();
-  std::chrono::duration<double> diff = e - s;
-  std::cout << "search time: " << diff.count() << "\n";
+	std::vector<std::vector<unsigned> > res(query_num);
+	for (unsigned i = 0; i < query_num; i++) res[i].resize(K);
 
-  save_result(argv[6], res);
+	auto s = std::chrono::high_resolution_clock::now();
+	for (unsigned i = 0; i < query_num; i++) {
+		index.SearchWithOptGraph(query_load + i * dim, K, paras, res[i].data());
+	}
+	auto e = std::chrono::high_resolution_clock::now();
+	std::chrono::duration<double> diff = e - s;
+	std::cout << "search time: " << diff.count() << "\n";
+	std::cout << "avarage latency: " << diff.count() / query_num << std::endl;
 
-  return 0;
+	save_result(argv[6], res);
+
+	if (argc == 8) {
+		std::vector<std::vector<int>> truths;
+		std::ifstream truthFile(argv[7], std::ios::binary);
+		while (true) {
+			int dim;
+			if (truthFile) {
+				truthFile.read(reinterpret_cast<char*>(&dim), 4);
+			}
+			else {
+				break;
+			}
+			std::vector<int> myvec(dim);
+			truthFile.read(reinterpret_cast<char*>(myvec.data()), 4 * dim);
+			truths.push_back(std::move(myvec));
+		}
+		truthFile.close();
+
+		float recall = 0;
+		for (int i = 0; i < res.size(); i++)
+		{
+			for (int id : res[i])
+			{
+				for (int j = 0; j < K; j++)
+				{
+					if (truths[i][j] == id)
+					{
+						recall++;
+						break;
+					}
+				}
+			}
+		}
+
+		recall = recall / static_cast<float>(res.size() * K);
+		
+		std::cout << "recall@" << K << ": " << recall << std::endl;
+	}
+
+	return 0;
 }
